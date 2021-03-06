4bb70b5a694c8183941e3fb6ecc097e84767cf6d
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-03-07 02:28:27 +0300

    Add files via upload

---
+++ Лаб.роб 5(С#)/Lab.rob5_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace Lab.rob5_1
+{
+    class Program
+    {
+        static void s(int k, int b=2, int c=7)
+        {
+                double x1; double x2;
+                double d = Math.Pow(b, 2) - 4 * k * c;
+                if (d < 0)
+                {
+                    Console.WriteLine("Квадратне рівняння не має коренів");
+                }
+                else
+                {
+                    if (d == 0) //квадратне рівняння має два однакові корені
+                    {
+                        x1 = -b / (2 * k);
+                        x2 = x1;
+                    }
+                    else //рівняння має два різні корені
+                    {
+                        x1 = (-b + Math.Sqrt(d)) / (2 * k);
+                        x2 = (-b - Math.Sqrt(d)) / (2 * k);
+                    Console.WriteLine($"x1 = {x1}; x2 = {x2}");
+                }
+                } 
+                static void Main(string[] args)
+                {
+                    Console.Write("a= ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.Write("b= ");
+                    int b = int.Parse(Console.ReadLine());
+                    s(a);
+                    s(b);
+                    Console.WriteLine("a={0}", a);
+                    Console.WriteLine("b={0}", b);
+
+                }
+              
+                
+                
+         
+        
+        
+        
+        }
+
+    
+    
+    
+    }
+
+ 
+
+
+}
---
+++ Лаб.роб 5(С#)/Lab.rob5_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab.rob5_3
+{
+    class Program
+    {
+        
+            /*Дано послідовність натуральних числень 
+            Використовуючипідпрограму, яка дозволяє встановити, чи є послідовність із чотирьох чисел 
+            арифметичною прогресією, знайти кількість послідовно розміщених четвірок чисел,які утворюють арифметичну прогресію.*/
+            static int x(int i)
+            {
+                if (i == 0)
+                {
+                    return 0;
+                }
+                else if (i == 1)
+                {
+                    return 7;
+                }
+                else
+                {
+                    return (x(i - 1) * (1 + x(i - 2)));
+                }
+            }
+            static void Main(string[] args)
+            {
+                Console.Write("n= ");
+                int n = int.Parse(Console.ReadLine());
+                n = x(n);
+                Console.WriteLine("n={0}", n);
+            }
+
+
+
+        
+
+
+
+
+    }
+
+
+
+
+}
