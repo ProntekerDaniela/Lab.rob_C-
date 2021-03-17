67761ec1bfbd3bb412af5e3b735c95d925199de2
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-03-17 17:06:34 +0300

    Add files via upload

---
+++ WinLab5/WindowsFormsAppLab5_1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppLab5_1
+{
+    public partial class Form1 : Form
+    {
+
+        private static double s (double k, double b = 2, double c = 7)
+        {
+            
+            double x1; double x2;
+            double d = Math.Pow(b, 2) - 4 * k * c;
+            if (d < 0)
+            {
+                return 0;
+
+            }
+            else
+            {
+                if (d == 0) //квадратне рівняння має два однакові корені
+                {
+                    x1 = -b / (2 * k);
+                    return x2 = x1;
+                }
+                else //рівняння має два різні корені
+                {
+                    return x1 = (-b + Math.Sqrt(d)) / (2 * k);
+                    return x2 = (-b - Math.Sqrt(d)) / (2 * k);
+                   
+
+                }
+            }
+
+
+        }
+
+
+            public Form1()
+            {
+            InitializeComponent();
+            }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a =Convert.ToDouble(textBox1.Text);
+            textBox1.Text = a.ToString();
+            double b =(int) Convert.ToDouble(textBox1.Text);
+            textBox1.Text = b.ToString();
+            a =s(a);
+            b = s(b);
+            textBox3.Text = a.ToString();
+            textBox4.Text = b.ToString();
+          
+        }
+    }
+}
---
+++ WinLab5/WindowsFormsAppLab5_3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppLab5_3
+{
+    public partial class Form1 : Form
+    {
+        static double x(double i)
+        {
+            if (i == 0)
+            {
+                return 0;
+            }
+            else if (i == 1)
+            {
+                return 7;
+            }
+            else
+            {
+                return (x(i - 1) * (1 + x(i - 2)));
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            double n = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = n.ToString();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double n = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = n.ToString();
+            n = x(n);
+            textBox2.Text = n.ToString();
+        }
+
+    }
+}
