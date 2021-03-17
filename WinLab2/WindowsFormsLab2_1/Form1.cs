29ada66688306b5112e0b0aeefadc66ff541f528
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-03-17 16:54:19 +0300

    Add files via upload

---
+++ WinLab2/WindowsFormsLab2_1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab2_1
+{
+    public partial class Form1 : Form
+    {
+        private static double m;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double a = Convert.ToDouble(textBox1.Text);
+            int n = Convert.ToInt32(textBox2.Text);
+            double s = 0;
+            double i = 0;
+            while (n >= i)
+            {
+                double k = (2 * n) / (a + n);
+                m = m + k;
+                i += 1;
+            }
+
+            textBox3.Text = m.ToString();
+        }
+    }
+}
---
+++ WinLab2/WindowsFormsLab2_3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab2_3
+{
+    public partial class Form1 : Form
+    {
+        private static int m;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double eps, x;
+
+            eps = Convert.ToDouble(textBox1.Text);
+            x = Convert.ToDouble(textBox2.Text);
+            double s = 0;
+            double n = 1;
+            double b = (1 - (4 * Math.Pow(x, 2)) / (Math.Pow(2 * n - 1, 2) * Math.Pow(Math.PI, 2)));
+            while (b >= eps)
+            {
+                b = (1 - (4 * Math.Pow(x, 2)) / (Math.Pow(2 * n - 1, 2) * Math.Pow(Math.PI, 2)));
+                if (-1 < m && m < 1)
+                {
+                    textBox3.Text = "Рівність справедлива";
+                }
+                else
+                {
+                    textBox3.Text = "Рівність несправедлива";
+                }
+            }
+
+
+
+
+        }
+    }
+}
