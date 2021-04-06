7311eeb90f6b135ed590eeaf49ca7f127f0828e7
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-04-06 22:15:16 +0300

    Add files via upload

---
+++ Лаб.роб 7(C#)/WindowsFormsLab.rob7/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsLab.rob7
+{
+    class Class1
+    {
+        public int A;
+        public int B;
+        public int C;
+        public int D;
+        
+
+        public Class1(int A, int B, int C, int D)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.D = D;
+           
+        }
+        public Class1(Class1 class1)
+        {
+            this.A = class1.A;
+            this.B = class1.B;
+            this.C = class1.C;
+            this.D = class1.D;
+            
+        }
+        public string __str__()
+        {
+            return $"{A}x+{B}y+{C}z+{D}";
+        }
+        public string input_and_output_of_coefficients()
+        {
+            return $"A:{A},B:{B},C:{C},D:{D}";
+        }
+        
+        public bool point(int x, int y, int z )
+        {
+            return (x >= 0) && (y >= 0) && (z >= 0);
+        }
+        public string projection(int x, int y, int z)
+        {
+            double t = -((A * x + B * y + C * z + D) / (Math.Pow(A, 2) + Math.Pow(B, 2) + Math.Pow(C, 2)));
+            double x1 = A * t + x;
+            double y1 = B * t + y;
+            double z1 = C * t + z;
+            return $"x1:{x1},y1:{y1},z1:{z1}";
+        }
+        public string crossing(int x, int y, int z, int l, int m, int n)
+        {
+            double k = -((A * x + B * y + C * z + D) / (A*l + B*m + C*n));
+            double x2 = l * k + x;
+            double y2 = m * k + y;
+            double z2 = n * k + z;
+            return $"x2:{x2},y2:{y2},z2:{z2}";
+        }
+        public bool parallel(int a, int b, int d, int e)
+        {  
+            return a / d == b / e;
+        }
+        
+    }
+
+}
---
+++ Лаб.роб 7(C#)/WindowsFormsLab.rob7/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab.rob7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 cl = new Class1(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+            listBox1.Items.Add (cl.input_and_output_of_coefficients());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Class1 cl = new Class1(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+            textBox8.Text = cl.point(Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox7.Text)).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Class1 cl = new Class1(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+            textBox15.Text = cl.parallel(Convert.ToInt32(textBox9.Text), Convert.ToInt32(textBox10.Text), Convert.ToInt32(textBox12.Text), Convert.ToInt32(textBox13.Text)).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Class1 cl = new Class1(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+            listBox2.Items.Add(cl.projection(Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox7.Text)));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Class1 cl = new Class1(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+            listBox3.Items.Add(cl.crossing(Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox16.Text), Convert.ToInt32(textBox17.Text), Convert.ToInt32(textBox18.Text)));
+        }
+    }
+}
