2923daa53e573095a3efe720d68a034b3e77635f
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-05-22 16:06:40 +0300

    Add files via upload

---
+++ Лаб. робота №14(С#)/DBSimulation/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Show(_db.GetAll());
+        }
+
+        private PcDatabase _db = new PcDatabase();
+        
+
+        private void Show(List<PC> list)
+        {
+            if (list.Count == 0)
+            {
+                MessageBox.Show("Введіть дані ");
+                return;
+            }
+            dataGridView1.RowCount = list.Count;
+            for (int i=0;i<list.Count;i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = list[i].name_surname;
+                dataGridView1.Rows[i].Cells[1].Value = list[i].work;
+                dataGridView1.Rows[i].Cells[2].Value = list[i].phone_number;
+                dataGridView1.Rows[i].Cells[3].Value = list[i].home_phone_number;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PC newPc = new PC(
+                textBoxName.Text,
+                textBoxWork.Text,
+                double.Parse(textBoxNumber1.Text),
+                double.Parse(textBoxNumber2.Text)
+                );
+            _db.Add(newPc);
+            Show(_db.GetAll());
+            tabControl1.SelectedIndex = 0;
+
+            StreamWriter sw = new StreamWriter("text.txt", true);
+            sw.WriteLine(textBoxName.Text);
+            sw.WriteLine(textBoxWork.Text);
+            sw.WriteLine(textBoxNumber1.Text);
+            sw.WriteLine(textBoxNumber2.Text);
+            sw.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentCell.RowIndex;
+            PC PcToDelete = _db.GetByIndex(i);
+            _db.Remove(PcToDelete);
+            Show(_db.GetAll());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+        }
+    
+    }
+}
---
+++ Лаб. робота №14(С#)/DBSimulation/PC.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSimulation
+{
+    class PC
+    {
+        
+        public string name_surname { get; set; }
+        public string work { get; set; }
+
+        private double _phone_number;
+        public double phone_number
+        {
+            get
+            {
+                return _phone_number;
+            }
+            set
+            {
+                if (value >= 0)
+                    _phone_number = value;
+                else
+                    throw new Exception("Номер телефону не може бути відємним");
+            }
+        }
+
+        private double _home_phone_number;
+        public double home_phone_number
+        {
+            get
+            {
+                return _home_phone_number;
+            }
+            set
+            {
+                if (value >= 0)
+                    _home_phone_number = value;
+                else
+                    throw new Exception("Домашній номер телефону не може бути відємним");
+            }
+        }
+
+      
+
+        public PC(string name_surname, string work, double phone_number, double home_phone_number)
+        {
+            this.name_surname = name_surname;
+            this.work = work;
+            this.phone_number = phone_number;
+            this.home_phone_number = home_phone_number;
+
+        }
+
+    }
+}
