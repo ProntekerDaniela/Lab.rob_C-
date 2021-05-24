3f4ea2710fa6927e92017829fa407fa420338d74
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-05-24 10:54:23 +0300

    Add files via upload

---
+++ C#/DBSimulation/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Show(_db.GetAll());
+        }
+
+        private PcDatabase _db = new PcDatabase();
+        
+
+        private void Show(List<PC> list)
+        {
+            if (list.Count == 0)
+            {
+                MessageBox.Show("Введіть дані ");
+                return;
+            }
+            dataGridView1.RowCount = list.Count;
+            for (int i=0;i<list.Count;i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = list[i].theme;
+                dataGridView1.Rows[i].Cells[1].Value = list[i].forum_content;
+                dataGridView1.Rows[i].Cells[2].Value = list[i].date_publication;
+                dataGridView1.Rows[i].Cells[3].Value = list[i].number_of_discussions;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PC newPc = new PC(
+                textBoxName.Text,
+                textBoxWork.Text,
+                double.Parse(textBoxNumber1.Text),
+                double.Parse(textBoxNumber2.Text)
+                );
+            _db.Add(newPc);
+            Show(_db.GetAll());
+            tabControl1.SelectedIndex = 0;
+
+            StreamWriter sw = new StreamWriter("text.txt", true);
+            sw.WriteLine(textBoxName.Text);
+            sw.WriteLine(textBoxWork.Text);
+            sw.WriteLine(textBoxNumber1.Text);
+            sw.WriteLine(textBoxNumber2.Text);
+            sw.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool f_open, f_save; // визначають чи вибрано файл і чи файл збережено
+            f_open = false; // файл не відкрито
+            f_save = false;
+            label1.Text = "-";
+            richTextBox1.Text = "";
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentCell.RowIndex;
+            PC PcToDelete = _db.GetByIndex(i);
+            _db.Remove(PcToDelete);
+            Show(_db.GetAll());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+        }
+    
+    }
+}
---
+++ C#/DBSimulation/PC.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSimulation
+{
+    
+    class PC
+    {
+        
+        public string theme { get; set; }
+        public string forum_content { get; set; }
+
+        private double _date_publication;
+        public double date_publication
+        {
+            get
+            {
+                return _date_publication;
+            }
+            set
+            {
+                if (value >= 0)
+                    _date_publication = value;
+                else
+                    throw new Exception("Дата публікації не може бути відємною");
+            }
+        }
+
+        private double _number_of_discussions;
+        public double number_of_discussions
+        {
+            get
+            {
+                return _number_of_discussions;
+            }
+            set
+            {
+                if (value >= 0)
+                    _number_of_discussions = value;
+                else
+                    throw new Exception("Кількість обговорень не може бути відємним");
+            }
+        }
+
+
+
+        public PC(string theme, string forum_content, double date_publication, double number_of_discussions)
+        {
+            this.theme = theme;
+            this.forum_content = forum_content;
+            this.date_publication = date_publication;
+            this.number_of_discussions = number_of_discussions;
+
+        }
+
+    }
+}
